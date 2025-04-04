1119c941f7297259f1a1e3e7a1df50f075818872
Vishal Nema <vishalnema23@gmail.com>
2025-04-05 01:54:01 +0530

    Set up messaging system, established peer-to-peer connection, and integrated video call functionality.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const path = require("path");
 
 const http = require("http");
 const socketIO = require("socket.io");
-
 const server = http.createServer(app);
 const io = socketIO(server);
 
@@ -16,21 +15,43 @@ io.on("connection", function (socket) {
   socket.on("joinroom", function () {
     if (waitingusers.length > 0) {
       let partner = waitingusers.shift();
-      const roomname = `$(socket.id)-${partner.id}`;
+      const roomname = `${socket.id}-${partner.id}`;
 
       socket.join(roomname);
       partner.join(roomname);
 
-      io.to(roomname).emit("joined");
+      io.to(roomname).emit("joined", roomname);
     } else {
       waitingusers.push(socket);
     }
   });
 
+  socket.on("signalingMessage", function (data) {
+    socket.broadcast.to(data.room).emit("signalingMessage", data.message);
+  });
+
+  socket.on("message", function (data) {
+    socket.broadcast.to(data.room).emit("message", data.message);
+  });
+
+  socket.on("startVideoCall", function ({ room }) {
+    socket.broadcast.to(room).emit("incomingCall");
+  });
+
+  socket.on("rejectCall", function ({ room }) {
+    socket.broadcast.to(room).emit("callRejected");
+  });
+
+  socket.on("acceptCall", function ({ room }) {
+    socket.broadcast.to(room).emit("callAccepted");
+  });
+
   socket.on("disconnect", function () {
     let index = waitingusers.findIndex(
-      (waitingUser) => waitingUser.id === socket.io
+      (waitingUser) => waitingUser.id === socket.id
     );
+
+    waitingusers.splice(index, 1);
   });
 });
 
@@ -40,4 +61,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-server.listen(3000);
+
+server.listen(process.env.PORT || 3000);
